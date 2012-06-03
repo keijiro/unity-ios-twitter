5b27da5cb8ac65e084a205a6d2267dfbc0308f3f
keijiro <keijiro@gmail.com>
2012-06-03 17:07:37 +0900

    Update the example.

--- Assets/Example/TweetComposer.js
+++ Assets/Example/TweetComposer.js
@@ -9,12 +9,12 @@ function Start() {
 function OnGUI() {
     if (availability) {
         if (GUI.Button(Rect(0, 0, Screen.width, 0.1 * Screen.height), "Send a tweet")) {
-            TwitterPlugin.ComposeTweet("これはツイートのテストです。", "http://www.example.com/sooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo_long_url");
+            TwitterPlugin.ComposeTweet("TEST テスト 실험 试验", "http://www.example.com/loooooooooooooooooooooooooooooooooooooong_url");
         }
         if (GUI.Button(Rect(0, 0.1 * Screen.height, Screen.width, 0.1 * Screen.height), "Send a tweet with a screenshot")) {
-            TwitterPlugin.ComposeTweetWithScreenshot("これはスクリーンショット付きツイートのテストです。", "http://www.example.com/sooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo_long_url");
+            TwitterPlugin.ComposeTweetWithScreenshot("TEST (with a screenshot)", "http://www.example.com/loooooooooooooooooooooooooooooooooooooong_url");
         }
     } else {
-        GUI.Label(Rect(0, 0, Screen.width, 0.15 * Screen.height), "Twitter function is not available.");
+        GUI.Label(Rect(0, 0, Screen.width, 0.15 * Screen.height), "Twitter API is not available.");
     }
 }
